8d459b422048fc4e29f84b10fda7dab250093fc9
Gustavo Barbosa <gustavocsb@gmail.com>
2019-08-13 19:55:33 -0400

    themes: wrap storybook with theme context provider

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 import { YellowBox } from 'react-native'
 import StorybookUI from './StorybookUI'
+import { Theme, ThemeContext } from '@loadsmart/blocks'
+
+const theme: Theme = {
+  primaryColor: '#14d64d',
+}
 
 class App extends Component {
   render() {
     YellowBox.ignoreWarnings(['Warning: Async Storage has been extracted from react-native core'])
-    return <StorybookUI />
+    return (
+      <ThemeContext.Provider value={theme}>
+        <StorybookUI />
+      </ThemeContext.Provider>
+    )
   }
 }
 
